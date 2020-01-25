2cc803f3c9ea84134bf63601a6ec4dccafc02671
webapis <webapis.github@gmail.com>
2020-01-25 15:09:33 +0500

    v

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,23 @@
 /* eslint-disable no-undef */
+
+const MongoClient = require('mongodb').MongoClient;
+const url =
+  'mongodb+srv://demoadmin:demoadmin@democluster-k12ir.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'demo';
 module.exports = (req, res) => {
-  const { name = 'World' } = req.query;
-  res.status(200).send(`Hello ${name}!`);
+  const client = new MongoClient(url);
+  // Use connect method to connect to the Server
+  client.connect(function(err) {
+    assert.equal(null, err);
+    console.log('Connected successfully to server');
+
+    const db = client.db(dbName);
+    db.collection('users').insertOne({
+      password: 'new pass',
+      email: 'webapis.github@gmail.com'
+    });
+    client.close();
+    const { name = 'World' } = req.query;
+    res.status(200).send(`Hello ${name}!`);
+  });
 };
