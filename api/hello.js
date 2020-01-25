0930da2562b788a493496894af0cc40d57a03c9a
webapis <webapis.github@gmail.com>
2020-01-25 15:45:57 +0500

    v

--- api/hello.js
+++ api/hello.js
@@ -1,25 +1,22 @@
 /* eslint-disable no-undef */
-
 const MongoClient = require('mongodb').MongoClient;
 const url =
   'mongodb+srv://demoadmin:demoadmin@democluster-k12ir.mongodb.net/test?retryWrites=true&w=majority';
 const dbName = 'demo';
-module.exports = (req, res) => {
-  const client = new MongoClient(url, { useUnifiedTopology: true });
-  // Use connect method to connect to the Server
-  client.connect(function(err) {
-    console.log('Mongo Error', err);
-    console.log('Connected successfully to server');
-
-    const db = client.db(dbName);
-    db.collection('users').insertOne({
+module.exports = async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = await client.db(dbName);
+    const result = await db.collection('users').insertOne({
       password: 'new pass',
       email: 'webapis.github@gmail.com'
     });
-    //  client.close();
-    const { name = 'World' } = req.query;
-    setTimeout(() => {
-      res.status(200).send(`Hello ${name}!`);
-    }, 10000);
-  });
+    console.log('inser result', result);
+    client.close();
+  } catch (error) {
+    console.log('mongodb Error');
+  }
+  const { name = 'World' } = req.query;
+
+  res.status(200).send(`Hello ${name}!`);
 };
