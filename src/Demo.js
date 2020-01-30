6b43436f7aba0a4323fefd7c5f9c470254d1c69b
webapis <webapis.github@gmail.com>
2020-01-30 12:01:12 +0500

    v

--- src/Demo.js
+++ src/Demo.js
@@ -2,19 +2,50 @@ import React from 'react';
 import Client from './Client';
 import usePusher, { getPusherConfig } from './signaling/pusher/usePusher';
 import ErrorMessage from './ErrorMessage';
-export default function Demo({ title, clientOneName, clientTwoName }) {
-  const { currentUser: clientOne, error: clientOneError,connectToPusher :connectOne,connecting:connectingOne,chatManager:chmOne } = usePusher(
-    getPusherConfig({ userId: clientOneName })
-  );
-  const { currentUser: clientTwo, error: clientTwoError,,connectToPusher :connectTwo,connecting:connectingTwo,chatManager:chmOne } = usePusher(
-    getPusherConfig({ userId: clientTwoName })
-  );
+import pusherIcon from './signaling/pusher/pusher.png';
+import './css/style.css';
 
+export default function Demo({ title, clientOneName, clientTwoName }) {
+  const {
+    currentUser: clientOne,
+    error: clientOneError,
+    connectToPusher: connectOne,
+    connecting: connectingOne
+  } = usePusher(getPusherConfig({ userId: clientOneName }));
+  const {
+    currentUser: clientTwo,
+    error: clientTwoError,
+    connectToPusher: connectTwo,
+    connecting: connectingTwo
+  } = usePusher(getPusherConfig({ userId: clientTwoName }));
+  function startDemo() {
+    connectOne();
+    connectTwo();
+  }
   if (clientOneError) {
     return <ErrorMessage error={clientOneError} />;
-  } else if (clientTwoError) {
+  }
+  if (clientTwoError) {
     return <ErrorMessage error={clientTwoError} />;
-  } else if (clientOne && clientTwo) {
+  }
+  if (!clientOne && !clientTwo && !connectingOne && !connectingTwo) {
+    return (
+      <div className="connection">
+        <button type="button" onClick={startDemo}>
+          Start Demo
+        </button>
+      </div>
+    );
+  }
+  if (connectingTwo || connectingOne) {
+    return (
+      <div className="connection">
+        <h3>Connecing.... to</h3>
+        <img width="100px" src={pusherIcon} alt="pusher icon" />
+      </div>
+    );
+  }
+  if (clientOne && clientTwo) {
     return (
       <div className="root">
         <h1 className="demo-title">{title}</h1>
